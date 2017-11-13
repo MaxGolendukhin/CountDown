1eba912a3b7a69fed75a2cfa62ed059cc3fdede3
MaximGolendukhin <maxwork82@gmail.com>
2017-11-13 22:31:14 +1000

    initial code

--- app/src/main/java/com/golendukhin/countdown/MainActivity.java
+++ app/src/main/java/com/golendukhin/countdown/MainActivity.java
@@ -1,5 +1,7 @@
 package com.golendukhin.countdown;
 
+import android.content.res.Configuration;
+import android.graphics.Color;
 import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -17,7 +19,14 @@ public class MainActivity extends AppCompatActivity {
         DateTime currentTime = new DateTime();
         DateTime newYearTime = new DateTime(2018, 2, 6, 0, 0);
         long interval = newYearTime.getMillis() - currentTime.getMillis();
-        final TextView textView = findViewById(R.id.elapsed_time_text_view);
+        final TextView countDownTextView = findViewById(R.id.elapsed_time_text_view);
+        TextView titleTextView = findViewById(R.id.title_text_view);
+
+        boolean landScapeOrientation = getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE;
+        if (landScapeOrientation) {
+            countDownTextView.setTextColor(Color.BLACK);
+            titleTextView.setTextColor(Color.BLACK);
+        }
 
         new CountDownTimer(interval, 1000) {
             public void onTick(long millisUntilFinished) {
@@ -29,7 +38,7 @@ public class MainActivity extends AppCompatActivity {
                 left %= 60 * 1000;
                 int seconds = (int)left / 1000;
 
-                textView.setText("time left" + "\n" + String.format(format, days, hours, minutes, seconds));
+                countDownTextView.setText("time left" + "\n" + String.format(format, days, hours, minutes, seconds));
             }
 
             public void onFinish() {}
